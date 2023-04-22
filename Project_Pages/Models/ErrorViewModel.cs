489f452e7eb5a0cc1e0be9c0019e8e13884ef323
Tudor Ada <Tudor.Io.Ada@student.utcluj.ro>
2023-04-22 12:14:56 +0300

    New proj MVC

---
+++ Project_Pages/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_Pages.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
