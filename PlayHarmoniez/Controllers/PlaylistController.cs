7fcc9e9428794cbe5e29a0db3cd5fa600dc3dec0
plescaevelyn <evelynplesca@gmail.com>
2023-04-28 19:42:25 +0300

    added controllers

--- PlayHarmoniez/Controllers/PlaylistController.cs
+++ PlayHarmoniez/Controllers/PlaylistController.cs
@@ -1,6 +1,78 @@
-﻿namespace PlayHarmoniez.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlayHarmoniez.App_Data;
+using PlayHarmoniez.Models;
+using System.Diagnostics;
+
+namespace PlayHarmoniez.Controllers
 {
-    public class PlaylistController
+    public class PlaylistController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly DataContext _dataContext;
+
+        public PlaylistController(ILogger<HomeController> logger, DataContext dataContext)
+        {
+            _logger = logger;
+            _dataContext = dataContext;
+        }
+
+        // TODO: modify this
+        public IActionResult Index()
+        {
+            var songs = _dataContext
+                .Songs
+                .Include(song => song.PlaylistSongs)
+                .ToList();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult AddPlaylist(DataContext dataContext, Playlist playlist)
+        {
+            _dataContext
+                .Playlist
+                .Add(playlist);
+
+            return View();
+        }
+
+        public IActionResult UpdatePlaylist(DataContext dataContext, Playlist playlist)
+        {
+            _dataContext
+                .Playlist
+                .Update(playlist);
+
+            return View();
+        }
+
+        public IActionResult DeletePlaylist(DataContext dataContext, Playlist playlist)
+        {
+            _dataContext
+                .Playlist
+                .Remove(playlist);
+
+            return View();
+        }
+
+        public IActionResult GetPlaylistById(DataContext dataContext, int id)
+        {
+            _dataContext
+                .Playlist
+                .SingleOrDefault(playlist => playlist.Id == id);
+
+            return View();
+        }
     }
 }
--- PlayHarmoniez/Controllers/UserController.cs
+++ PlayHarmoniez/Controllers/UserController.cs
@@ -1,6 +1,73 @@
 ﻿namespace PlayHarmoniez.Controllers
 {
-    public class UserController
+    using global::PlayHarmoniez.App_Data;
+    using global::PlayHarmoniez.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using System.Diagnostics;
+
+    namespace PlayHarmoniez.Controllers
     {
+        public class UserController : Controller
+        {
+            private readonly ILogger<HomeController> _logger;
+            private readonly DataContext _dataContext;
+
+            public UserController(ILogger<HomeController> logger, DataContext dataContext)
+            {
+                _logger = logger;
+                _dataContext = dataContext;
+            }
+
+            // TODO: add index if needed
+
+            public IActionResult Privacy()
+            {
+                return View();
+            }
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            public IActionResult Error()
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+
+            public IActionResult AddUser(DataContext dataContext, User user)
+            {
+                _dataContext
+                    .Users
+                    .Add(user);
+
+                return View();
+            }
+
+            public IActionResult UpdateUser(DataContext dataContext, User user)
+            {
+                _dataContext
+                    .Users
+                    .Update(user);
+
+                return View();
+            }
+
+            public IActionResult DeleteUser(DataContext dataContext, User user)
+            {
+                _dataContext
+                    .Users
+                    .Remove(user);
+
+                return View();
+            }
+
+            public IActionResult GetUsersById(DataContext dataContext, int id)
+            {
+                _dataContext
+                    .Users
+                    .SingleOrDefault(user => user.Id == id);
+
+                return View();
+            }
+        }
     }
+
 }
