c14d39aa35a65282edfbd3c209ac6b892d17b83f
plescaevelyn <evelynplesca@gmail.com>
2023-05-13 23:17:50 +0300

    added files back

---
+++ PlayHarmoniez/Models/Song.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlayHarmoniez.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishData { get; set; }
+        public string Description { get; set; }
+        public int AlbumId { get; set; }
+        public Album Album { get; set; }
+        public string SoundFile { get; set; }
+        public string ImageFile { get; set; }
+        public List<PlaylistSong> PlaylistSongs { get; set; }
+        public List<LikedSong> LikedSong { get; set; }
+    }
+}
